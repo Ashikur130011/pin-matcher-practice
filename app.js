4c3380c8ebbf6a71d9001e7e80a7a3ac2a915af0
Ashikur Rahman <smashikurrahman24@gmail.com>
2022-06-18 10:37:38 +0600

    finished

--- app.js
+++ app.js
@@ -14,6 +14,28 @@ function getPin() {
 }
 document.getElementById('key-pad').addEventListener('click', function(event){
     const num =  event.target.innerText
-    const inputNumber = document.getElementById('input-number')
-    inputNumber.value = num
+    const inputNumber = document.getElementById("input-number");
+    if(isNaN(num)){
+      if(num == 'C'){
+        inputNumber.value = ''
+      }
+    }
+    else{
+      const previousNum = inputNumber.value;
+      const newNum = previousNum + num;
+      inputNumber.value = newNum;
+    }
+})
+document.getElementById('submit-btn').addEventListener('click', function(){
+  
+  const generatePin = document.getElementById('input-form').value
+  const inputNumber = document.getElementById('input-number').value
+  if(generatePin ==  inputNumber){
+    document.getElementById('successful-msg').style.display= 'block'
+    document.getElementById('warning-msg').style.display= 'none'
+  }
+  else{
+    document.getElementById("successful-msg").style.display = "none";
+    document.getElementById("warning-msg").style.display = "block";
+  }
 })
